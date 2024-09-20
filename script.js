a2024adea460e0e869b5d897a422579de907216a
Sparsh <105844638+SparshMah@users.noreply.github.com>
2024-09-20 22:46:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     if (localStorage.getItem("data")){
+//     listContainer.innerHTML = localStorage.getItem("data");
+// }
+// });
